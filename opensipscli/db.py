a5a2a0f4ecd05d63f7c3481e6fc8f1375d220ac1
Razvan Crainea <razvan@opensips.org>
2019-03-06 10:21:18 +0200

    db: add insert, update, delete commands

--- opensipscli/db.py
+++ opensipscli/db.py
@@ -63,6 +63,23 @@ class osdb(object):
             raise osdbError("cannot handle {} dialect".
                     format(self.dialect)) from None
 
+    def get_where(self, filter_keys):
+
+        if filter_keys:
+            where_str = ""
+            for k, v in filter_keys.items():
+                where_str += " AND {} = ".format(k)
+                if type(v) == int:
+                    where_str += v
+                else:
+                    where_str += "'{}'".format(
+                            v.translate(str.maketrans({'\'': '\\\''})))
+            if where_str != "":
+                where_str = " WHERE " + where_str[5:]
+        else:
+            where_str = ""
+        return where_str
+
     def exists(self):
         # TODO: do this only for SQLAlchemy
         if not self.conn:
@@ -106,6 +123,7 @@ class osdb(object):
         self.conn.execute("CREATE DATABASE {}".format(self.db_name))
 
     def use(self):
+        # TODO: do this only for SQLAlchemy
         self.conn.execute("USE {}".format(self.db_name))
 
     def create_module(self, import_file):
@@ -120,3 +138,92 @@ class osdb(object):
                 raise osdbError("cannot deploy {} file: {}".
                         format(import_file, ie)) from None
 
+    def find(self, table, fields, filter_keys):
+        # TODO: do this only for SQLAlchemy
+        if not self.conn:
+            raise osdbError("connection not available")
+        if not fields:
+            fields = ['*']
+        elif type(fields) != list:
+            fields = [ fields ]
+
+        where_str = self.get_where(filter_keys)
+        statement = "SELECT {} FROM {}{}".format(
+                ", ".join(fields),
+                table,
+                where_str)
+        try:
+            result = self.conn.execute(statement)
+        except sqlalchemy.exc.SQLAlchemyError as ex:
+            logger.error("cannot execute query: {}".format(statement))
+            logger.error(ex)
+            return None
+        return result
+
+    def entry_exists(self, table, constraints):
+        ret = self.find(table, "count(*)", constraints)
+        if ret and ret.first()[0] != 0:
+            return True
+        return False
+
+    def insert(self, table, keys):
+        # TODO: do this only for SQLAlchemy
+        if not self.conn:
+            raise osdbError("connection not available")
+
+        values = ""
+        for v in keys.values():
+            values += ", "
+            if type(v) == int:
+                values += v
+            else:
+                values += "'{}'".format(
+                        v.translate(str.maketrans({'\'': '\\\''})))
+        statement = "INSERT INTO {} ({}) VALUES ({})".format(
+                table, ", ".join(keys.keys()), values[2:])
+        try:
+            result = self.conn.execute(statement)
+        except sqlalchemy.exc.SQLAlchemyError as ex:
+            logger.error("cannot execute query: {}".format(statement))
+            logger.error(ex)
+            return False
+        return True
+
+    def delete(self, table, filter_keys=None):
+        # TODO: do this only for SQLAlchemy
+        if not self.conn:
+            raise osdbError("connection not available")
+
+        where_str = self.get_where(filter_keys)
+        statement = "DELETE FROM {}{}".format(table, where_str)
+        try:
+            result = self.conn.execute(statement)
+        except sqlalchemy.exc.SQLAlchemyError as ex:
+            logger.error("cannot execute query: {}".format(statement))
+            logger.error(ex)
+            return False
+        return True
+
+    def update(self, table, update_keys, filter_keys=None):
+        # TODO: do this only for SQLAlchemy
+        if not self.conn:
+            raise osdbError("connection not available")
+
+        update_str = ""
+        for k, v in update_keys.items():
+            update_str += ", {} = ".format(k)
+            if type(v) == int:
+                update_str += v
+            else:
+                update_str += "'{}'".format(
+                        v.translate(str.maketrans({'\'': '\\\''})))
+        where_str = self.get_where(filter_keys)
+        statement = "UPDATE {} SET {}{}".format(table,
+                update_str[2:], where_str)
+        try:
+            result = self.conn.execute(statement)
+        except sqlalchemy.exc.SQLAlchemyError as ex:
+            logger.error("cannot execute query: {}".format(statement))
+            logger.error(ex)
+            return False
+        return True
