1c4ecc19e6d599dc6bf6d71eef4eb3d754ca232e
Razvan Crainea <razvan@opensips.org>
2019-04-23 19:35:53 +0300

    logger: add interface to use the colors

--- opensipscli/logger.py
+++ opensipscli/logger.py
@@ -23,8 +23,6 @@ logger.py - implements coloured logging for the opensips-cli project
 
 import logging
 
-BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
-
 #These are the sequences need to get colored ouput
 RESET_SEQ = "\033[0m"
 COLOR_SEQ = "\033[1;%dm"
@@ -37,30 +35,14 @@ def formatter_message(message, use_color = True):
         message = message.replace("$RESET", "").replace("$BOLD", "")
     return message
 
-COLORS = {
-    'WARNING': YELLOW,
-    'INFO': WHITE,
-    'DEBUG': BLUE,
-    'CRITICAL': YELLOW,
-    'ERROR': RED
-}
-
-class ColoredFormatter(logging.Formatter):
-    def __init__(self, msg, use_color = True):
-        logging.Formatter.__init__(self, msg)
-        self.use_color = use_color
-
-    def format(self, record):
-        levelname = record.levelname
-        if self.use_color and levelname in COLORS:
-            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
-            record.levelname = levelname_color
-        return logging.Formatter.format(self, record)
-
 # Custom logger class with multiple destinations
 class ColoredLogger(logging.Logger):
+
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
     FORMAT = "$BOLD%(levelname)s$RESET: %(message)s"
     COLOR_FORMAT = formatter_message(FORMAT, True)
+
     def __init__(self, name):
         logging.Logger.__init__(self, name)
 
@@ -71,6 +53,31 @@ class ColoredLogger(logging.Logger):
 
         self.addHandler(console)
         return
+
+    def color(self, color, message):
+        return COLOR_SEQ % (30 + color) + message + RESET_SEQ
+
+class ColoredFormatter(logging.Formatter):
+
+    LEVELS_COLORS = {
+        'WARNING': ColoredLogger.YELLOW,
+        'INFO': ColoredLogger.WHITE,
+        'DEBUG': ColoredLogger.BLUE,
+        'CRITICAL': ColoredLogger.YELLOW,
+        'ERROR': ColoredLogger.RED
+    }
+
+    def __init__(self, msg, use_color = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in self.LEVELS_COLORS:
+            levelname_color = COLOR_SEQ % (30 + self.LEVELS_COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
 
 logging.setLoggerClass(ColoredLogger)
 logger = logging.getLogger(__name__)
