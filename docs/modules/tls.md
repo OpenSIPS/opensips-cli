a7388bde965e17f29c937a8ace8b23237e341f3b
Liviu Chircu <liviu@opensips.org>
2020-04-23 16:15:26 +0300

    TLS module: Improve usability for `userCERT`
        * now 100% functional by default, no more random exceptions on
            missing config file options
        * more suggestive, less cryptic text prompts
        * deprecate the `tls_user_config` logic
            (any settings should go in cli.cfg)
        * add better defaults for each setting
        * update documentation
        * improve code quality
    
    Fixes #48

--- docs/modules/tls.md
+++ docs/modules/tls.md
@@ -4,10 +4,9 @@ Using the `tls` module, you can generate TLS certificates and private keys.
 
 The module has two subcommands:
 * `rootCA` - generates a CA (certification authority) self signed certificate
-and private key pair
-* `userCERT` - generates a certificate which is signed by rootCA
-and assigned to an user. Moreover, this command creates a private key for that
-user and a ca-list(chain of trust in TSL)
+and private key pair.  These are to be used by a TLS server.
+* `userCERT` - generates a certificate signed by a given CA, a private key and
+a CA list (chain of trust) file.  These are to be used by TLS clients (users).
 
 ## Configuration
 
@@ -29,24 +28,24 @@ List of `opensips-cli.cfg` settings for configuring self-signed CA certificates:
 * tls_ca_key_size - the size of the RSA key, in bits (e.g. 4096)
 * tls_ca_md - the digest algorithm to use for signing (e.g. SHA1)
 
-List of parameters for configuring user certificate:
-* tls_user_common_name - the common name
-* tls_user_dir - the director where user files  will be stored
-* tls_user_cacert - the path to the rootCA certificate
-* tls_user_cakey - the path to the rootCA private key
-* tls_user_cert_file - the location of user's certificate inside the ca dir
-* tls_user_key_file - the location of user's private key  inside the ca dir
-* tls_user_calist_file - location of user's ca trust chain inside the ca dir
-* tls_user_key_size - the size of the RSA key
-* tls_user_country - the initials of the country(example: 'ro') 
-* tls_user_state - the state
-* tls_user_city - the city
-* tls_user_organisation - the name of the organisation
-* tls_user_organisational_unit - the organisation unit
-* tls_user_md - the md used for signing
-* tls_user_notafter - the valadity period
-* tls_user_serial - the serial for the user certificate
-* tls_user_overwrite - set this to y if you want to overwrite cert,n otherwise
+List of `opensips-cli.cfg` settings for configuring user certificates:
+
+* tls_user_dir - output directory where the cert and key will be written to
+* tls_user_cert_file - output certificate file path, within `tls_user_dir`
+* tls_user_key_file - output private key file path, within `tls_user_dir`
+* tls_user_calist_file - output CA list file path, within `tls_user_dir`
+* tls_user_overwrite - set this to "y" in order to overwrite existing files
+* tls_user_cacert - path to the input CA certificate
+* tls_user_cakey - path to the input CA private key
+* tls_user_common_name - the address of the website (e.g. "www.opensips.org")
+* tls_user_country - the initials of the country (e.g. "RO")
+* tls_user_state - the state (e.g. "Bucharest")
+* tls_user_locality - the city (e.g. "Bucharest")
+* tls_user_organisation - the name of the organisation (e.g. "OpenSIPS")
+* tls_user_organisational_unit - the organisational unit (e.g. "Project")
+* tls_user_notafter - the validity period, in seconds (e.g. 315360000)
+* tls_user_key_size - the size of the RSA key, in bits (e.g. 4096)
+* tls_user_md - the digest algorithm to use for signing (e.g. SHA1)
 
 
 ## Examples
@@ -73,28 +72,28 @@ tls_ca_key_size: 4096
 tls_ca_md: SHA1
 ```
 
-To create a user certificate signed by rootCA and private key and a ca-list(chain of trust in TSL):
+To create a user certificate signed by the above rootCA, along with a private
+key and a CA list (chain of trust) file:
 ```
 opensips-cli -x tls userCERT
 ```
 Configuration file example for userCERT:
 ```
 [default]
-tls_user_common_name: www.open.com
 tls_user_dir: /etc/opensips/tls/user
-tls_user_cacert: /etc/opensips/tls/rootCA/cacert.pem
-tls_user_cakey: /etc/opensips/tls/rootCA/private/cakey.pem
 tls_user_cert_file: user-cert.pem
 tls_user_key_file: user-privkey.pem
 tls_user_calist_file: user-calist.pem
-tls_user_key_size: 4096
-tls_user_country: ro
-tls_user_state: Braila
-tls_user_city: Braila
-tls_user_organisation: Opensips
-tls_user_organisational_unit: Solutions
-tls_user_md: sha1
-tls_user_notafter: 315360000
-tls_user_serial: 2
 tls_user_overwrite: yes
+tls_user_cacert: /etc/opensips/tls/rootCA/cacert.pem
+tls_user_cakey: /etc/opensips/tls/rootCA/private/cakey.pem
+tls_user_common_name: www.opensips.org
+tls_user_country: RO
+tls_user_state: Bucharest
+tls_user_locality: Bucharest
+tls_user_organisation: OpenSIPS
+tls_user_organisational_unit: Project
+tls_user_notafter: 315360000
+tls_user_key_size: 4096
+tls_user_md: SHA1
 ```
--- opensipscli/modules/tls.py
+++ opensipscli/modules/tls.py
@@ -99,80 +99,84 @@ class tls(Module):
         logger.info("CA private key created in " + k_f)
 
     def do_userCERT(self, params):
-        tlscfg = cfg
-        user_cfg = cfg.get("tls_user_config")
-        if user_cfg:
-            tlscfg = OpenSIPSCLIConfig()
-            tlscfg.parse(user_cfg)
+        global cfg
+        logger.info("Preparing to generate user cert + key + CA list...")
 
-        cn = tlscfg.read_param("tls_user_common_name", "input the hostname of the website the certificate is for", "www.open.ro")
-        user_dir = tlscfg.read_param("tls_user_dir", "user director", "/etc/opensips/tls/user/")
-        cert_file = tlscfg.read_param("tls_user_cert_file","cert_file", "user-cert.pem")
-        key_file = tlscfg.read_param("tls_user_key_file","key_file", "user-privkey.pem")
-        calist_file = tlscfg.read_param("tls_user_calist_file","cert_file", "user-calist.pem")
+        # TODO
+        # separate cli.cfg files for TLS are fully deprecated, this if block is
+        # only kept for backwards-compatibility.  Remove starting from v3.2! <3
+        if cfg.exists('tls_user_config'):
+            tls_cfg = cfg.get('tls_user_config')
+            cfg = OpenSIPSCLIConfig()
+            cfg.parse(tls_cfg)
+
+        user_dir = cfg.read_param("tls_user_dir", "Output directory", "/etc/opensips/tls/user/")
+        cert_file = cfg.read_param("tls_user_cert_file", "Output cert file", "user-cert.pem")
+        key_file = cfg.read_param("tls_user_key_file", "Output key file", "user-privkey.pem")
+        calist_file = cfg.read_param("tls_user_calist_file", "Output CA list file", "user-calist.pem")
+
         c_f = join(user_dir, cert_file)
         k_f = join(user_dir, key_file)
         ca_f = join(user_dir, calist_file)
-        
-        create_cert = False
-        if not exists(c_f) or not exists(k_f):
-            create_cert = True
-        else:
-            if tlscfg.read_param("tls_user_overwrite", "user certificate already exists, do you want to overwrite it?", "yes", True):
-                create_cert = True
-        if create_cert:
-            cacert = tlscfg.read_param("tls_user_cacert","cert_file", "/etc/opensips/tls/rootCA/cacert.pem")
-            cakey = tlscfg.read_param("tls_user_cakey","key_file", "/etc/opensips/tls/rootCA/private/cakey.pem")
-            ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(cacert, 'rt').read())
-            ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, open(cakey, 'rt').read())
-            
-            # create a key pair
-            key = crypto.PKey()
-            key_size = int(tlscfg.read_param("tls_user_key_size","key_size", 4096))
-            key.generate_key(crypto.TYPE_RSA, key_size)
-            
-            # create a self-signed cert
-            cert = crypto.X509()
 
-            cert.get_subject().C = tlscfg.read_param("tls_user_country", "country", "ro")
-            cert.get_subject().ST = tlscfg.read_param("tls_user_state", "state", "Braila")
-            cert.get_subject().L = tlscfg.read_param("tls_user_city", "city", "Braila")
-            cert.get_subject().O = tlscfg.read_param("tls_user_organisation", "organization", "opensips")
-            cert.get_subject().OU = tlscfg.read_param("tls_user_organisational_unit", "organisational unit", "project")
-            cert.get_subject().CN = cn 
-            serial = int(tlscfg.read_param("tls_user_serial", "serial", 2))
-            cert.set_serial_number(serial)
-            cert.gmtime_adj_notBefore(0)
-            notafter = int(tlscfg.read_param("tls_user_notafter", "duration", 315360000))
-            cert.gmtime_adj_notAfter(notafter)
-            cert.set_issuer(ca_cert.get_subject())
-            cert.set_pubkey(key)
-            md = tlscfg.read_param("tls_user_md", "md", "sha1")
-            cert.sign(ca_key, md)
+        if (exists(c_f) or exists(k_f) or exists(ca_f)) and not cfg.read_param("tls_user_overwrite",
+                "User certificate, key or CA list file already exists, overwrite?", "yes", True):
+            return
 
+        cacert = cfg.read_param("tls_user_cacert", "CA cert file", "/etc/opensips/tls/rootCA/cacert.pem")
+        cakey = cfg.read_param("tls_user_cakey", "CA key file", "/etc/opensips/tls/rootCA/private/cakey.pem")
+        ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, open(cacert, 'rt').read())
+        ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, open(cakey, 'rt').read())
+
+        # create a self-signed cert
+        cert = crypto.X509()
+
+        cert.get_subject().CN = cfg.read_param("tls_user_common_name", "Website address (CN)", "www.opensips.org")
+        cert.get_subject().C = cfg.read_param("tls_user_country", "Country (C)", "RO")
+        cert.get_subject().ST = cfg.read_param("tls_user_state", "State (ST)", "Bucharest")
+        cert.get_subject().L = cfg.read_param("tls_user_locality", "Locality (L)", "Bucharest")
+        cert.get_subject().O = cfg.read_param("tls_user_organisation", "Organization (O)", "OpenSIPS")
+        cert.get_subject().OU = cfg.read_param("tls_user_organisational_unit", "Organisational Unit (OU)", "Project")
+
+        cert.set_serial_number(randrange(100000))
+        cert.gmtime_adj_notBefore(0)
+        notafter = int(cfg.read_param("tls_user_notafter", "Certificate validity (seconds)", 315360000))
+        cert.gmtime_adj_notAfter(notafter)
+        cert.set_issuer(ca_cert.get_subject())
+
+        # create a key pair
+        key = crypto.PKey()
+        key_size = int(cfg.read_param("tls_user_key_size", "RSA key size (bits)", 4096))
+        key.generate_key(crypto.TYPE_RSA, key_size)
+
+        cert.set_pubkey(key)
+        md = cfg.read_param("tls_user_md", "Digest Algorithm", "SHA1")
+        cert.sign(ca_key, md)
+
+        try:
             if not exists(dirname(c_f)):
                 makedirs(dirname(c_f))
-            try:
-                open(c_f, "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode('utf-8'))
-            except Exception as e:
-                logger.error(e)   
+            open(c_f, "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode('utf-8'))
+        except Exception as e:
+            logger.error(e)
 
+        try:
             if not exists(dirname(k_f)):
                 makedirs(dirname(k_f))
-            try:
-                open(k_f, "wt").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key).decode('utf-8'))
-            except Exception as e:
-                logger.error(e)   
-            
+            open(k_f, "wt").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key).decode('utf-8'))
+        except Exception as e:
+            logger.error(e)
+
+        try:
             if not exists(dirname(ca_f)):
                 makedirs(dirname(ca_f))
-            try:
-                open(ca_f, "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, ca_cert).decode('utf-8'))
-            except Exception as e:
-                logger.error(e)   
-            logger.info("user certificate created in " + c_f)
-            logger.info("user private key created in " + k_f)
-            logger.info("user calist(ca chain of trust) created in " + ca_f)
+            open(ca_f, "wt").write(crypto.dump_certificate(crypto.FILETYPE_PEM, ca_cert).decode('utf-8'))
+        except Exception as e:
+            logger.error(e)
+
+        logger.info("user certificate created in " + c_f)
+        logger.info("user private key created in " + k_f)
+        logger.info("user CA list (chain of trust) created in " + ca_f)
 
 
     def __exclude__(self):
